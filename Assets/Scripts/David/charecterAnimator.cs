302322353d4753f8a1d4f6c4fa924a393b4d82a8
Bread <redatonedash@gmail.com>
2019-10-03 11:33:01 -0700

    new stuff

---
+++ Assets/Scripts/David/charecterAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charecterAnimator : MonoBehaviour
+{
+    public Transform cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, cam.eulerAngles.y, transform.eulerAngles.z);
+    }
+}
