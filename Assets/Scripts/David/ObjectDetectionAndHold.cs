6e0d2c1a43136be9890c05b9057357bb6a6a9831
Bread <redatonedash@gmail.com>
2019-10-11 14:41:58 -0700

    just pushing untested stuff no big deal

---
+++ Assets/Scripts/David/ObjectDetectionAndHold.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDetectionAndHold : MonoBehaviour
+{
+    [SerializeField]
+    Animator doorAnimatior;
+    [SerializeField]
+    GameObject[] acceptedObjects;
+    List<GameObject> penetratingObjects = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.attachedRigidbody.isKinematic = true;
+        OpenDoor();
+    }
+
+    public void OpenDoor()
+    {
+        Debug.Log("Open Door");
+        doorAnimatior.SetBool("Open", true);
+    }
+
+    public void CloseDoor()
+    {
+        Debug.Log("Close Door");
+        doorAnimatior.SetBool("Open", false);
+    }
+}
