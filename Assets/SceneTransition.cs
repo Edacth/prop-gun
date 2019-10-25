2b80226be358f36b206761425c4e839193580ab8
Cade Anderson <cadeanderson99@gmail.com>
2019-10-25 10:45:02 -0700

    Add SceneTransition trigger

---
+++ Assets/SceneTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField]
+    string targetScene = "Showcase";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("TRANSITION");
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+}
