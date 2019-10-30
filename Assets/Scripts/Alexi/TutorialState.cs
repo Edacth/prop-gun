7fe29393e4b73417e5d2310c5665cebcafd7b314
Alexi Most <alexibmost@gmail.com>
2019-10-30 16:52:22 -0700

    event system

---
+++ Assets/Scripts/Alexi/TutorialState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialState : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(KeyValuePair<PhysicsGun.Mode, PhysicsEffect> effectPair in PhysicsGun.effects)
+        {
+            effectPair.Value.effectAppliedEvent += Thing;
+        }
+    }
+
+    public void Thing()
+    {
+        Debug.Log("Thing");
+    }
+}
